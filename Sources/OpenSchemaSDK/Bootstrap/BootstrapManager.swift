0f6e8f80758dab85aa6036bac8fc89a8cf6616b3
Rodrigo Saravia <rodsaravia@hotmail.com>
2020-11-20 02:57:13 -0800

    Fixed path for rootca on Bootstrap3 try

--- Sources/OpenSchemaSDK/Bootstrap/BootstrapManager.swift
+++ Sources/OpenSchemaSDK/Bootstrap/BootstrapManager.swift
@@ -60,9 +60,9 @@ public class BootstrapManager {
     private func BootstrapLogic() {
         //Step i: get certificate; Tutorial at: https://medium.com/@ambrose12silveira/ios-swift-grpc-integration-with-tls-client-authentication-f2e2164ed125
         let certificateFileName = "rootca"
-        //let certificateFilePath = Bundle(for: NSClassFromString("BootstrapManager")!).path(forResource: certificateFileName, ofType: "pem")
-        print(Bundle.main)
-        let certificateFilePath = Bundle.main.path(forResource: certificateFileName, ofType: "pem")
+        let certificateFilePath = Bundle(for: self).path(forResource: certificateFileName, ofType: "pem")
+        print(certificateFilePath)
+        //let certificateFilePath = Bundle.main.path(forResource: certificateFileName, ofType: "pem")
         
         do {
             let pemCert = try NIOSSLCertificate.fromPEMFile(certificateFilePath!)
