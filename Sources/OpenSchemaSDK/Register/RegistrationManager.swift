a5faed8127d3b7350df1130d1956ccbb4316f63e
Rodrigo Saravia <rodsaravia@hotmail.com>
2020-11-20 02:26:58 -0800

    Added Bootstrap and Registration server certificates, Added Basic Login to Username and Password to RegistrationManager.swift

--- Sources/OpenSchemaSDK/Register/RegistrationManager.swift
+++ Sources/OpenSchemaSDK/Register/RegistrationManager.swift
@@ -42,41 +42,50 @@ public class RegistrationManager {
     public func registerDevice() {
 
         let Url = String(format: self.registerServerAddress)
-            guard let serviceUrl = URL(string: Url) else { return }
-            let parameters: [String: Any] = [
-                "uuid" : uuidManager.getUUID(),
-                "publicKey": self.trimPublicKeyPEMString
-            ]
-            var request = URLRequest(url: serviceUrl)
-            request.httpMethod = "POST"
-            request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
-            guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
-                return
+        
+        guard let serviceUrl = URL(string: Url) else { return }
+        let parameters: [String: Any] = [
+            "uuid" : uuidManager.getUUID(),
+            "publicKey": self.trimPublicKeyPEMString
+        ]
+            
+        let username = "Dw5QIUx0Ric2yLo7IogrfHZGiJ6s3gC+FQaekCw3pY8/FlG+9g+Xp8Fo1fuADpRhBmQNsyeuhmjbq+A4QEz1VLeyHFHkNilhQE9NsUArSC1UjyiO/CY01vxVaIcydOSW
+        let password = " 4fvgk4YJGeJYWYL/FVld3gKs0FXHhQ3KBD5yKtg7h/7IlH3o+C/kuCnyjFT8RTXTYBPl4n01scp1/LMgh8mUhY0ZxCJek48Fguig5PZVIoy7yozB+f8oROASiFrBji6u"
+        let loginString = String(format: "%@:%@", username, password)
+        let loginData = loginString.data(using: String.Encoding.utf8)!
+        let base64LoginString = loginData.base64EncodedString()
+            
+        var request = URLRequest(url: serviceUrl)
+        request.httpMethod = "POST"
+        request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
+        request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
+            return
+        }
+        request.httpBody = httpBody
+        request.timeoutInterval = 20
+        let session = URLSession(configuration: URLSessionConfiguration.ephemeral, delegate: NSURLSessionPinningDelegate(), delegateQueue: nil)
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
             }
-            request.httpBody = httpBody
-            request.timeoutInterval = 20
-            let session = URLSession(configuration: URLSessionConfiguration.ephemeral, delegate: NSURLSessionPinningDelegate(), delegateQueue: nil)
-            session.dataTask(with: request) { (data, response, error) in
-                if let response = response {
-                    print(response)
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
                 }
-                if let data = data {
-                    do {
-                        let json = try JSONSerialization.jsonObject(with: data, options: [])
-                        print(json)
-                    } catch {
-                        print(error)
-                    }
-                }
-            }.resume()
+            }
+        }.resume()
     }
     
-
 }
 
 ///This class pins the server certificate to verify the server origin.
 class NSURLSessionPinningDelegate: NSObject, URLSessionDelegate {
     
+    private let certificateFolderPath = "Sources/OpenSchemaSDK/Register/"
     private let serverCertificateName : String = "server"
     private let serverCertificateExtension : String = "der"
     
@@ -91,7 +100,7 @@ class NSURLSessionPinningDelegate: NSObject, URLSessionDelegate {
         
         var remoteCertMatchesPinnedCert = false
         
-        if let myCertPath = Bundle.main.path(forResource: self.serverCertificateName, ofType: self.serverCertificateExtension) {
+        if let myCertPath = Bundle.main.path(forResource: self.certificateFolderPath + self.serverCertificateName, ofType: self.serverCertificateExtension) {
             if let pinnedCertData = NSData(contentsOfFile: myCertPath) {
                 
                 // Compare certificate data
